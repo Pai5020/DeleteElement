540b1fa5f6a2ddb3b2558812524a2a581bb681dd
PARVATHY SUGUNAN <parvathysugunan5020@gmail.com>
2024-04-29 11:03:04 +0530

    again committed

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int i,j, element;
-        int[] arr = new int[6];
+        int[] arr = new int[9];
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the array elements : ");
         for (i = 0; i < arr.length; i++) {
@@ -12,12 +12,19 @@ public class Main {
         System.out.println("Enter the element to be deleted : ");
         element = sc.nextInt();
         int size = arr.length;
-        for (i = 0,j=0; i < size; i++) {
+        j=0;
+        int count = 0;
+        for (i = 0; i < size; i++) {
                 if (arr[i] != element) {
                     arr[j] = arr[i];
                     j++;
                 }
-            }
+                else if(arr[i] == element){
+                    arr[i] = 0;
+                }
+
+        }
         System.out.println("The array after deletion : "+ Arrays.toString(arr));
     }
+
 }
